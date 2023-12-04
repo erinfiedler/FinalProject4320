858c289a833adbde0ec4ef3676ff98292ff54eef
Isaac <isaacrivera117.gmail.com>
2023-12-04 15:07:05 -0600

    Initicalized web page

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return "main page"
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home"))
 
 print("Welcome to the Trip Reservation System")
 
