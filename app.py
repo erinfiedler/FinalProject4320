faf65c58bbeadf72c83bfc7aa7ecb828ce47cd09
Erin <eefnmc@umsystem.edu>
2023-12-07 10:33:46 -0600

    cleaning up

--- app.py
+++ app.py
@@ -89,23 +89,6 @@ def get_cost_matrix():
     cost_matrix = [[100, 75, 50, 100] for row in range(12)]
     return cost_matrix
 
-#display seating chart
-# def seating_chart():
-#     cost_matrix = get_cost_matrix()
-#     reservations = load_reservations()
-
-#     print("Seating Chart:")
-#     for row_num, row in enumerate(cost_matrix, start=1):
-#         for col_num, seat_cost in enumerate(row, start=1):
-#             # Check if the seat is reserved
-#             reserved = any(row == seat_row and col == seat_col for _, seat_row, seat_col, _ in reservations)
-            
-#             if reserved:
-#                 print(f"X", end=" ")  # Mark reserved seats with 'X'
-#             else:
-#                 print(f"{row_num}-{col_num}(${seat_cost})", end=" ")  # Display seat number and cost
-
-#         print()  # Move to the next row
 
 # Inside your Flask route or function
 @app.route('/seating_chart')
